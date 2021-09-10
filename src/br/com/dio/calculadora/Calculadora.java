b742682f6b8b0e2d7abee4f9bb3a92531ecba86d
Wesley Lins <weslins22@gmail.com>
2021-09-10 17:45:17 -0300

    Organização do código

--- src/br/com/dio/calculadora/Calculadora.java
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -6,19 +6,19 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		int a, b;
-		
+
 		System.out.println("Digite o primeiro valor: ");
 		a = scan.nextInt();
 		System.out.println("Digite o segundo valor: ");
 		b = scan.nextInt();
 
-		int soma = soma(a,b);
-		int subtracao = subtracao(a,b);
-		int multiplicacao = multiplicacao(a,b);
-		int divisao = divisao(a,b);
-		
+		int soma = soma(a, b);
+		int subtracao = subtracao(a, b);
+		int multiplicacao = multiplicacao(a, b);
+		int divisao = divisao(a, b);
+
 		System.out.println("soma: " + soma);
 		System.out.println("sub: " + subtracao);
 		System.out.println("mult: " + multiplicacao);
@@ -28,14 +28,17 @@ public class Calculadora {
 	public static int soma(int a, int b) {
 		return a + b;
 	}
+
 	public static int subtracao(int a, int b) {
 		return a - b;
 	}
+
 	public static int divisao(int a, int b) {
 		return a / b;
 	}
+
 	public static int multiplicacao(int a, int b) {
 		return a * b;
 	}
-	
+
 }
